d0e054d91486dc3c6c462f585cc8578066d3a900
kungbot <planmine722@gmail.com>
2024-10-14 17:33:02 +0100

    first commit

--- Start/Files/CreateFiles/Program.cs
+++ Start/Files/CreateFiles/Program.cs
@@ -5,6 +5,17 @@ const string filename = "TestFile.txt";
 
 // TODO: Create a new file - this will overwrite any existing file
 // Use the "using" construct to automatically close the file stream
-
-
-// TODO: Determine if a file exists
+using (StreamWriter sw = File.CreateText(filename)) {
+sw.WriteLine("This is a text file.");
+}
+Console.WriteLine(File.Exists(filename));
+if (File.Exists(filename)) {
+    // The Delete function deletes a file
+    File.Delete(filename);
+}
+else {
+    using (StreamWriter sw = File.CreateText(filename)) {
+        sw.WriteLine("This is a text file.");
+     }
+}
+Console.WriteLine(File.Exists(filename));
--- Start/Files/Directories/Program.cs
+++ Start/Files/Directories/Program.cs
@@ -4,21 +4,25 @@
 const string dirname = "TestDir";
 
 // TODO: Create a Directory if it doesn't already exist
-
-
-// TODO: Get the path for the current directory
-
-
-// TODO: Just like with files, you can retrieve info about a directory
-
-
-// TODO: Enumerate the contents of directories
-// Console.WriteLine("Just directories:");
-
-// Console.WriteLine("---------------");
-
-// Console.WriteLine("Just files:");
-
-// Console.WriteLine("---------------");
-
-// Console.WriteLine("All directory contents:");
+  if (!Directory.Exists(dirname)) {
+Directory.CreateDirectory(dirname);
+}
+else {
+Directory.Delete(dirname);
+}
+// Get the path for the current directory
+string curpath = Directory.GetCurrentDirectory();
+Console.WriteLine($"Current directory is {curpath}");
+// Just like with files, you can retrieve info about a directory
+DirectoryInfo di = new DirectoryInfo(curpath);
+Console.WriteLine($"{di.Name}");
+Console.WriteLine($"{di.Parent}");
+Console.WriteLine($"{di.CreationTime}");
+Console.WriteLine("---------------");
+// Enumerate the contents of directories
+Console.WriteLine("Just directories:");
+List<string> thedirs = new List<string>(Directory.EnumerateDirectories(curpath));
+foreach (string dir in thedirs) {
+Console.WriteLine(dir);
+}
+Console.WriteLine("---------------");
